b443a4c9a1f5b1b828f2bed5835c538a6d292769
CIINY <65286862+CIINY@users.noreply.github.com>
2021-03-11 17:41:16 +0900

    Add files via upload

---
+++ 0310/OneDim.java
@@ -0,0 +1,18 @@
+package week2;
+
+public class OneDim {
+
+	public static void main(String[] args) {
+		int sale[] = new int[] {157, 209, 251, 312};
+		/*
+		int sale[] = new int[4];
+		sale[0] = 152;		sale[1] = 209;
+		sale[2] = 251;		sale[3] = 312; 
+		*/
+		for(int i = 0; i < 4; i++) {
+			System.out.println(sale[i]);
+		}
+	
+	}
+	
+}
---
+++ 0310/TwoDim.java
@@ -0,0 +1,23 @@
+package week2;
+
+public class TwoDim {
+
+	public static void main(String[] args) {
+//		int sale[][] = new int[][] {{63, 84, 140, 130}, {157, 209, 251, 312}};
+		int sale[][] = new int[][] {{1, 2, 3, 4}, {5, 6, 7, 8}};
+		
+//		for(int i=0; i<2; i++) {
+//			for(int j=0; j<4; j++) {
+//				System.out.printf("%d/4ºÐ±â : sale[%d][%d]= %d %n", j+1,i,j,sale[i][j]);
+//				System.out.println();
+//			}
+//		}
+		for(int i=0; i<2; i++) {
+			for(int j=0; j<4; j++) {
+				System.out.println(sale[i][j]);
+			}
+		}
+
+	}
+
+}
